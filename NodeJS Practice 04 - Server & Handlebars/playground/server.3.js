29dacda3ce24fcb11228fbc69810e5a08f2bcdd2
BernardoMB <bmondragonbrozon@gmail.com>
2020-08-10 20:29:24 -0500

    Updated for better classes

--- NodeJS Practice 04 - Server & Handlebars/playground/server.3.js
+++ NodeJS Practice 04 - Server & Handlebars/playground/server.3.js
@@ -1,21 +1,21 @@
 // Use of a middleware function to display the help.html page.
-
 const express = require('express');
+const path = require('path'); // native module: function to manipulate file or directory paths
 
 var app = express();
 
-// In order to run the help page. I need a middleware function called use.
-app.use(express.static('./public'));
+// Configura an entire directory to serve static files. (Serve the files in public)
+// Express cant serve static files providing relative path. It need the absolute path.
+console.log(__dirname);
+console.log(__filename);
 
-app.get('/', (req, res) => {
-    res.send({
-        name: 'Bernardo',
-        likes: [
-            'football',
-            'math'
-        ]
-    });
-});
+// Point to the public folder
+console.log(path.join(__dirname, '../public'));
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+// In order to run the help page. I need a middleware function called use.
+app.use(express.static(publicDirectoryPath));
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
--- NodeJS Practice 04 - Server & Handlebars/playground/server.4.js
+++ NodeJS Practice 04 - Server & Handlebars/playground/server.4.js
@@ -1,33 +1,27 @@
 // Rendering a page on views.
 
+const path = require('path'); // native module: function to manipulate file or directory paths
 const express = require('express');
 const hbs = require('hbs'); // Templateing
 
 var app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
 // Set some express configurations:
 // Tell express what view engige I want to use.
 app.set('view engine', 'hbs');
 // The default directory express use for our templates is 'views'.
 
+// This is very useful because with this I can recicle elements such as header and footer
+
 // In order to run the help page, I need a middleware function called 'use'.
-app.use(express.static('./public'));
+app.use(express.static(publicDirectoryPath));
 
-// Routeing:
-
-app.get('/', (req, res) => {
-    res.send({
-        name: 'Bernardo',
-        likes: [
-            'football',
-            'math'
-        ]
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Some title weather app',
+        name: 'Bernardo'
     });
-});
-
-app.get('/about', (req, res) => {
-    // Render the about page.
-    res.render('about.hbs')
 });
 
 app.listen(3000, () => {
